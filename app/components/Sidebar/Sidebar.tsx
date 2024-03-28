c56b2c847e166b7f7a1f244f8e2c3c2b081812a8
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-03-28 13:23:40 +0300

    feat(sidebar): create a logout button

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -6,8 +6,13 @@ import Image from "next/image";
 import menu from "@/app/utils/menu";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import Button from "../Button/Button";
+import { logout } from "@/app/utils/Icons";
+import { useClerk } from "@clerk/nextjs";
 
 const Sidebar = () => {
+  const { signOut } = useClerk();
+  const router = useRouter();
   const { theme } = useGlobalState();
   const Router = useRouter();
   const pathname = usePathname();
@@ -42,7 +47,18 @@ const Sidebar = () => {
           );
         })}
       </ul>
-      <button></button>
+      <div className="sign-out relative m-6">
+        <Button 
+          name={"Sign Out"}
+          type={"submit"}
+          padding={"0.4rem 0.8rem"}
+          borderRad={"0.8rem"}
+          fw={"500"}
+          fs={"1.2rem"}
+          icon={logout}
+          click={() => signOut(() => router.push("/signin"))}
+        />
+      </div>
     </SidebarStyled>
   );
 };
