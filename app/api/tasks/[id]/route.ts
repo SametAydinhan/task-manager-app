2134e00aad3e9a5dcf6269d8757cf92b80af0752
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-03-29 14:35:04 +0300

    feat(route[id]): backend deleting handle

---
+++ app/api/tasks/[id]/route.ts
@@ -0,0 +1,27 @@
+import prisma from "@/app/utils/connect";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { id } = params;
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const task = await prisma.task.delete({
+      where: {
+        id,
+      },
+    });
+    return NextResponse.json(task);
+  } catch (error) {
+    console.log("ERROR DELETE TASK: ", error);
+    return NextResponse.json({ error: "Error deleting task" }, { status: 500 });
+  }
+}
