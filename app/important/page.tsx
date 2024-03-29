943419ea210b7cefdd95358a095041f1e6440f70
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-03-29 14:39:43 +0300

    feat(important): important page done

--- app/important/page.tsx
+++ app/important/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 import React from 'react'
+import { useGlobalState } from '../context/globalProvider';
+import Tasks from '../components/Tasks/Tasks';
 
 const page = () => {
+  const {importantTasks} = useGlobalState();
   return (
-    <div>important</div>
+    <Tasks title='Important Tasks' tasks={importantTasks}/>
   )
 }
 
