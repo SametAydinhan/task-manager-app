e437764971b098346f333c599d8ae7182dbf114e
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-03-29 14:34:00 +0300

    feat(glovalprovider): add complete incomplete and important filter

--- app/context/globalProvider.js
+++ app/context/globalProvider.js
@@ -3,11 +3,13 @@ import React, { createContext, use, useContext, useState } from "react";
 import themes from "./themes";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { useUser } from "@clerk/nextjs";
 
 export const GlobalContext = createContext();
 export const GlobalUpdateContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
+  const { user } = useUser();
   const [selectedtheme, setSelectedTheme] = useState(0);
   const [isloading, setIsLoading] = useState(false);
 
@@ -25,12 +27,29 @@ export const GlobalProvider = ({ children }) => {
     }
   };
 
-  React.useEffect(() => {
-    allTasks();
-  }, []);
+  const deleteTask = async (id) => {
+    setIsLoading(true);
+    try {
+      const res = await axios.delete(`/api/tasks/${id}`);
+      toast.success("Task deleted successfully");
+      allTasks();
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong!");
+    }
+  };
 
+  React.useEffect(() => {
+    if (user) allTasks();
+  }, [user]);
+
+  const completedTasks = tasks.filter((task) => task.isCompleted === true);
+  const importantTasks = tasks.filter((task) => task.isImportant === true);
+  const incompletedTasks = tasks.filter((task) => task.isCompleted === false);
   return (
-    <GlobalContext.Provider value={{ theme, tasks }}>
+    <GlobalContext.Provider
+      value={{ theme, tasks, deleteTask, isloading, completedTasks,importantTasks,incompletedTasks }}
+    >
       <GlobalUpdateContext.Provider value={{}}>
         {children}
       </GlobalUpdateContext.Provider>
