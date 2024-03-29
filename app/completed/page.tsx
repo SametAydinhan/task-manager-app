eda0ed966c5bc8abf8c9b7e2dda0eaae3ca5eb26
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-03-29 14:37:43 +0300

    feat(completed): completed page done

--- app/completed/page.tsx
+++ app/completed/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import React from "react";
+import { useGlobalState } from "../context/globalProvider";
+import Tasks from "../components/Tasks/Tasks";
 
 const page = () => {
-  return <div>Complated</div>;
+  const {completedTasks} = useGlobalState();
+  return <Tasks title="Completed Tasks" tasks={completedTasks}/>;
 };
 
 export default page;
