019e642a2b98c3a04fa5771732a54969229b3063
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-03-29 14:41:09 +0300

    fix(contextprovider):add a loader

--- app/providers/ContextProvider.tsx
+++ app/providers/ContextProvider.tsx
@@ -14,8 +14,7 @@ const ContextProvider = ({ children }: Props) => {
       setIsReady(true);
     }, 200);
   }, []);
-  if(!isReady) return null;
-
+  if(!isReady){ return (<div className="w-full h-full flex items-center justify-center"><span className="loader"></span></div>);}
   return <GlobalProvider>
     <Toaster />
     {children}
